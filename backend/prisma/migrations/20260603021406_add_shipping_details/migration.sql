1c53dde7d221951d78444967b27b91ed994f5be7
dineshctoula <ds9806026041@gmail.com>
2026-06-03 07:59:51 +0545

    feat(database): add shipping fields to Order model and run migration

---
+++ backend/prisma/migrations/20260603021406_add_shipping_details/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - Added the required column `shippingAddress` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `shippingCity` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `shippingCountry` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `shippingPostalCode` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "totalAmount" REAL NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "shippingAddress" TEXT NOT NULL,
+    "shippingCity" TEXT NOT NULL,
+    "shippingPostalCode" TEXT NOT NULL,
+    "shippingCountry" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Order" ("createdAt", "id", "status", "totalAmount", "updatedAt", "userId") SELECT "createdAt", "id", "status", "totalAmount", "updatedAt", "userId" FROM "Order";
+DROP TABLE "Order";
+ALTER TABLE "new_Order" RENAME TO "Order";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
